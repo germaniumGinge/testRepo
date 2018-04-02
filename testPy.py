4ea45aa277b6f1a23923e88dddf1c77a297e3914
germaniumGinge <siliconginge@gmail.com>
2018-04-02 16:14:48 +0000

    Test for master branch

--- testPy.py
+++ testPy.py
@@ -31,16 +31,4 @@ while (1):
 	else:
 		print("You are not hungry enough, scrub.")
 	
-	valid = 0
-	while (valid == 0):
-		valid = 1
-		areLion = input ("Are you a lion? Y/N")
-		if areLion == 'y' or areLion == 'Y':
-			print("Glad to see a fellow lion of culture round these parts")
-		elif areLion == 'n' or areLion == 'N':
-			print("TO THE BALLPIT WITH YOUUUU")
-		else:
-			valid = 0
-			print("Invalid input")
-		
-	
+	print ("This is a merger test, watch out friendos")
